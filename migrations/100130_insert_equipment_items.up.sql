001534ebca2d7cdd0a2d5ad10e07a9438b9f6a5e
Rob Doneux <robertdoneux@gmail.com>
2025-12-29 20:43:56 +0000

    feat(migrations): add initial equipment items and their descriptions to the database

---
+++ migrations/100130_insert_equipment_items.up.sql
@@ -0,0 +1,310 @@
+INSERT INTO items (
+    name,
+    description,
+    weight,
+    price,
+    item_type,
+    can_be_equipped
+) VALUES 
+    (
+        'Backpack',
+        'A sturdy pack for carrying gear. Can hold up to 30 lbs of items.',
+        1.0,
+        2,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Back of light tools',
+        'Contains small tongs, pliers, screwdriver, small hammer, small pry bar, lockpicks, 3m string, .09m of wire and miscellaneous screws and nails.',
+        1.0,
+        10,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Back of heavy tools',
+        'Contains hammer, six spikes, crowbar, large tongs, chisel and 3m of light (but strong)metal cable.',
+        1.0,
+        12,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Bedroll',
+        'A compact sleeping mat and blanket for one person.',
+        1.0,
+        2,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Book',
+        'Provides an asset to any roll made concerning the subject of the book as long as the character has the book in their possession, can read it, and can devot half an hour to the task (or twice as long as normal, whichever is greater).',
+        1.0,
+        20,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Burlap sack',
+        'A simple sack made of rough fabric.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Compass',
+        'A small device that shows direction relative to magnetic north.',
+        1.0,
+        10,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Crowbar',
+        'A sturdy metal bar with a flattened, curved end for prying things open.',
+        1.0,
+        2,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Disguise kit',
+        'Includes cosmetics, facial prostheses, and a few other tricks. Provides an asset to disguise tasks, lowering the difficulty by one step when you attempt to disguise yourself as another person',
+        1.0,
+        12,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Explorer''s pack',
+        'Contains 15m of rope, rations for three days, three spikes, hammer, warm clothes, sturdy boots, three torches, and two minor glowglobes',
+        1.0,
+        20,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'First aid kit',
+        'Contains bandages, a few tools, salves, and so on. Provides an asset to healing tasks, lowering the difficulty by one step when you attempt to heal yourself or another person',
+        1.0,
+        10,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Grappling hook',
+        'A metal hook attached to a length of rope, used for climbing or securing the rope to a surface.',
+        1.0,
+        3,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Lockpicks',
+        'A set of small tools used to pick locks.',
+        1.0,
+        5,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Matchsticks',
+        'A small bundle of wooden sticks used to start fires.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Musical Instrument',
+        'A simple musical instrument, such as a flute or drum.',
+        1.0,
+        10,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Pouch',
+        'A small bag for carrying coins or other small items.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Rope (15 meters)',
+        'A length of sturdy rope, useful for climbing or tying things together.',
+        1.0,
+        2,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Shield',
+        'Asset for Speed defence tasks (decreases the diffuculty by one step).',
+        1.0,
+        3,
+        'EQUIPMENT',
+        TRUE
+    ),
+    (
+        'Tent',
+        'Large enough to accommodate two people',
+        1.0,
+        3,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Torch',
+        'A wooden stick with a flammable material at one end, used to provide light.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Ale',
+        'A pint of alcoholic beverage.',
+        1.0,
+        3,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Wine',
+        'A bottle of fermented grape juice.',
+        1.0,
+        3,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Boots',
+        'Sturdy footwear for adventuring.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Cigarettes',
+        'A pack of tobacco cigarettes.',
+        1.0,
+        2,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Clothing',
+        'A set of everyday clothes suitable for various environments.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Meal',
+        'A standard meal for one person.',
+        1.0,
+        1,
+        'EQUIPMENT',
+        FALSE
+    ),
+    (
+        'Rations (1 day)',
+        'Packaged food sufficient to sustain one person for a day.',
+        1.0,
+        2,
+        'EQUIPMENT',
+        FALSE
+    );
+
+INSERT INTO equipment (
+    item_id
+) 
+VALUES 
+    (
+        (SELECT id FROM items WHERE name = 'Backpack')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Back of light tools')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Back of heavy tools')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Bedroll')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Book')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Burlap sack')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Compass')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Crowbar')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Disguise kit')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Explorer''s pack')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'First aid kit')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Grappling hook')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Lockpicks')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Matchsticks')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Musical Instrument')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Pouch')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Rope (15 meters)')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Shield')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Tent')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Torch')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Ale')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Wine')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Boots')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Cigarettes')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Clothing')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Meal')
+    ),
+    (
+        (SELECT id FROM items WHERE name = 'Rations (1 day)')
+    );
